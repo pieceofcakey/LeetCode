b2404026a10bf612f56198256c9b89814c629042
Seoho Choi <102589090+pieceofcakey@users.noreply.github.com>
2024-04-12 23:22:44 +0900

    Time: 50 ms (88.42%), Space: 49 MB (90.41%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+const addBinary = function(a, b) {
+    const sum = BigInt(`0b${a}`) + BigInt(`0b${b}`);
+    
+    return sum.toString(2)
+};
