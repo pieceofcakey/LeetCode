e420ebd4daa7f5c850c9971ae0e420506e46e256
Seoho Choi <devseoho@gmail.com>
2024-07-05 14:40:35 +0900

    Time: 77 ms (60.33%), Space: 67.4 MB (92.46%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function (root) {
+  if (root === null) {
+    return 0;
+  }
+
+  let left = root.left;
+  let right = root.right;
+
+  let leftDepth = 0,
+    rightDepth = 0;
+
+  while (left) {
+    left = left.left;
+    leftDepth++;
+  }
+
+  while (right) {
+    right = right.right;
+    rightDepth++;
+  }
+
+  if (leftDepth === rightDepth) {
+    return (2 << leftDepth) - 1;
+  }
+
+  return countNodes(root.left) + countNodes(root.right) + 1;
+};
